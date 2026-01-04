5628939e2a94bfb4dbe744993f3aec2f88814136
th-arq <y02luv@gmail.com>
2026-01-04 18:19:23 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  
   /* =========================
      Body Fade In（div不要）
-  ========================= */
+  ========================= 
   document.body.style.opacity = '0';
   document.body.style.transition = 'opacity 0.8s ease';
 
   requestAnimationFrame(() => {
     document.body.style.opacity = '1';
   });
-
+*/
 
   /* =========================
      Hamburger Menu
