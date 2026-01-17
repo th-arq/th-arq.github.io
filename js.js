c31f252b40656d9d680c1700040e63977ce89754
th-arq <y02luv@gmail.com>
2026-01-17 14:54:49 +0900

    Update js.js

--- js.js
+++ js.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let currentScroll = 0;
 let targetScroll = 0;
-const ease = 0.10; // 小さいほど余韻長い（0.08〜0.15おすすめ）
+const ease = 0.05; // 小さいほど余韻長い（0.08〜0.15おすすめ）
 
 function inertiaScroll() {
   targetScroll = window.scrollY;
