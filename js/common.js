ee0903224a4e02fb488c049b59e561c6b3bcfccf
th-arq <y02luv@gmail.com>
2026-02-23 17:01:59 +0900

    Create common.js

---
+++ js/common.js
@@ -0,0 +1,11 @@
+fetch('head.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('header').innerHTML = html;
+  });
+
+fetch('foot.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('footer').innerHTML = html;
+  });
