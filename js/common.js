551be11a4bc2052e95be0a470f0c5b1ee4ca5882
th-arq <y02luv@gmail.com>
2026-02-27 22:38:49 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -9,3 +9,30 @@ fetch('../foot.html')
   .then(html => {
     document.getElementById('footer').innerHTML = html;
   });
+
+
+  const panels = document.querySelectorAll('.panel');
+const title = document.querySelector('.title-text');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const text = entry.target.dataset.title;
+
+        // 一旦消す → 入れ替え → 下から表示
+        title.classList.remove('show');
+
+        setTimeout(() => {
+          title.textContent = text;
+          title.classList.add('show');
+        }, 200);
+      }
+    });
+  },
+  {
+    threshold: 0.6
+  }
+);
+
+
