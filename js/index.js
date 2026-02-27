9489a4b5ab2df2c1119f1a8a31ff2ba706fe4e2e
th-arq <y02luv@gmail.com>
2026-02-27 19:01:29 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -36,3 +36,15 @@ window.addEventListener("load", () => {
     document.body.classList.remove("is-lock");
   }, 1600);
 });
+
+fetch('head.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('header').innerHTML = html;
+  });
+
+fetch('foot.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('footer').innerHTML = html;
+  });
