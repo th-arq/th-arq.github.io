29e416978f3ab79e22e4028c1f2bda7fb7749214
th-arq <y02luv@gmail.com>
2026-02-27 23:37:50 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,19 +1,14 @@
 window.addEventListener("load", () => {
 
-  // header
+  // ===== header =====
   fetch('/head.html')
     .then(res => res.text())
     .then(html => {
       const header = document.getElementById('header');
-      if (header) {
-        header.innerHTML = html;
-
-        // header 読み込み完了を通知
-        document.dispatchEvent(new Event('headerLoaded'));
-      }
+      if (header) header.innerHTML = html;
     });
 
-  // footer
+  // ===== footer =====
   fetch('/foot.html')
     .then(res => res.text())
     .then(html => {
@@ -22,30 +17,3 @@ window.addEventListener("load", () => {
     });
 
 });
-
-const panels = document.querySelectorAll('.panel');
-const titles = document.querySelectorAll('.title-item');
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const current = entry.target.dataset.title;
-
-        titles.forEach(title => {
-          title.classList.toggle(
-            'is-active',
-            title.dataset.title === current
-          );
-        });
-      }
-    });
-  },
-  {
-    root: null,
-    rootMargin: '-40% 0px -40% 0px',
-    threshold: 0
-  }
-);
-
-panels.forEach(panel => observer.observe(panel));
