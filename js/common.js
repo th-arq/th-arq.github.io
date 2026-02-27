5e87dee0a0865431f7ee44dbd062b30a024a7e9b
th-arq <y02luv@gmail.com>
2026-02-27 22:50:39 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,16 +1,3 @@
-fetch('../head.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('header').innerHTML = html;
-  });
-
-fetch('../foot.html')
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById('footer').innerHTML = html;
-  });
-
-
 const panels = document.querySelectorAll('.panel');
 const title = document.querySelector('.title-text');
 
