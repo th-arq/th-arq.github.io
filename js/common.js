5628376673dc4e0f931139c14b9e3a3c0bc062bd
th-arq <y02luv@gmail.com>
2026-02-27 19:01:52 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,10 +1,10 @@
-fetch('head.html')
+fetch('../head.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById('header').innerHTML = html;
   });
 
-fetch('foot.html')
+fetch('../foot.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById('footer').innerHTML = html;
