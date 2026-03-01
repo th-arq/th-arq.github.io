d15a5a672bc1217940bfc0d216c6229de97d650a
th-arq <y02luv@gmail.com>
2026-03-01 14:57:44 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -9,4 +9,23 @@ window.addEventListener("load", () => {
   loadParts('header', '/head.html');
   loadParts('footer', '/foot.html');
 
+  // ---  Lenis (Smooth Scroll)  ---
+  if (typeof Lenis !== 'undefined') {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    // Unload for Index 
+    if (document.body.classList.contains('is-index')) {
+      lenis.stop();
+    }
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }
 });
